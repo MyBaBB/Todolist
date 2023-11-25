58dd8f2caddddbd0f94217153525d0e6cfb7072a
Brett Baker <GitBaBB@MyBaBB.com>
2023-11-25 09:07:08 -0800

    tailwind configured and css configuredsaome  Google fonts installed.

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,11 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react'
-
+import '../Test.css'
 const Test = () => {
   return (
-    <div className='testout text-red-700  '>
+ <div className='testout'>
       Hello World !!
-    </div>
+    <div className='Creepster' >Creepster</div>
+    <div className='Itim' >Itim</div>
+    <div className='Kranky' >Kranky</div>
+    <div className='LibreBaskerville' >LibreBaskerville</div>
+    <div className='PTserif' >PT serif</div>
+    <div className='Rye' >Rye</div> 
+</div>
   )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,43 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+      varDARKBEAKTOP:        "#785001",  
+      varMEDBEAKTOP:         "#b67902", 
+      varLIGHTBEAKTOP:       "#fca802", 
+    
+      varDARKDUCKHEAD:       "#1e3b28",       
+      varMEDDUCKHEAD:        "#284f35",    
+      varLIGHTDUCKHEAD:      "#528f85", 
+      
+      varDARKBLUEFEATHER:    "#24558d",  
+      varMEDBLUEFEATHER:     "#2a61a1",  
+      varLIGHTBLUEFEATHER:   "#419aff", 
+      
+      varDARKDUCKCHEEKS:     "#362e47", 
+      varMEDDUCKCHEEKS:      "#6b5b8d", 
+      varLIGHTDUCKCHEEKS:    "#a088d2",
+      
+      varDARKNECK:           "#412a32",  
+      varMEDNECK:            "#98648d", 
+      varLIGHTNECK:          "#c68199", 
+     
+      varDARKWING:           "#3c302e", 
+      varMEDWING:            "#806661", 
+      varLIGHTWING:          "#cfa69d", 
+    
+      varDARKBEAK:           "#70181a", 
+      varMEDBEAK:            "#962023",
+      varLIGHTBEAK:          "#d72d32", 
+    
+      varDARKSPACE:          "#08121c", 
+      varMEDSPACE:           "#1b3854", 
+      varLIGHTSPACE:         "#264d73", 
+
+
+    },
   },
   plugins: [],
 }
-
+}
